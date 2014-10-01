a85ff4e307727398296073a2b96dc8ee9572c606
Steven Jourdan Djamil <jrdn.sx@gmail.com>
2014-10-01 10:19:05 +0700

    Initial Update Dependencies

---
+++ NIDCWA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NIDCWA.Startup))]
+namespace NIDCWA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
